a8601c88ec3fd54381edfa37d370525cfaa539db
Cataldo Mazzilli <cm@zextras.com>
2023-10-30 17:19:44 +0100

    WIP: improve grid

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,19 @@ import {ListHeader} from "./components/ListHeader.tsx";
 import styled from "styled-components";
 
 const Grid = styled.div`
+  box-sizing: border-box;
   width: 100%;
-  gap: 0.25rem 1rem;
-  padding: 0 1.5rem; 
+  gap: 0.5rem 1rem;
   display: grid;
-  grid-template-columns: fit-content() auto repeat(3, 1fr);
+  grid-template-columns: max-content 3fr repeat(3, minmax(max-content, 1fr));
   justify-items: start;
+  align-items: center;
+  grid-template-rows: minmax(max-content, 1fr);
 `;
 
 const RowBorder = styled(Divider)`
-    grid-column: 1 / 5; /* this code makes the row stretch to entire width of the container */
+  grid-column: 1 / span 5; /* this code makes the row stretch to entire width of the container */
 `;
-
 
 
 const rows = [
@@ -32,22 +33,23 @@ const rows = [
 
 function App() {
 
-  const rowsWithDividers = rows.map((value) => <><ListItem {...value}/><RowBorder color={'secondary.disabled'} /></>)
+    const rowsWithDividers = rows.map((value) => <><ListItem {...value}/><RowBorder
+        color={'secondary.disabled'}/></>)
 
 
-  return (
-      <ThemeProvider>
-          <div style={{width: '800px'}}>
-              <HeaderBreadcrumbs crumbs={crumbsBuilder(10)} />
-          </div>
-          <ListHeader />
-          <Grid>
-              {rowsWithDividers}
-          </Grid>
+    return (
+        <ThemeProvider>
+            <HeaderBreadcrumbs crumbs={crumbsBuilder(10)}/>
+            <Grid>
+                <RowBorder color={'secondary.disabled'}/>
+                <ListHeader/>
+                <RowBorder color={'secondary.disabled'}/>
+                {rowsWithDividers}
+            </Grid>
 
 
-      </ThemeProvider>
-  )
+        </ThemeProvider>
+    )
 }
 
 export default App
--- src/components/HeaderBreadcrumbs.tsx
+++ src/components/HeaderBreadcrumbs.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {Breadcrumbs, BreadcrumbsProps} from "@zextras/carbonio-design-system";
+import {Breadcrumbs, BreadcrumbsProps, Container} from "@zextras/carbonio-design-system";
 
 interface HeaderBreadcrumbsProps {
     crumbs: BreadcrumbsProps['crumbs']
 }
 
 export const HeaderBreadcrumbs: React.FC<HeaderBreadcrumbsProps> = ({crumbs}) => {
-    return <Breadcrumbs crumbs={crumbs} />;
+    return <Container padding={{all: '1rem'}} width={'100vw'} background={'gray5'}> <Breadcrumbs crumbs={crumbs} /></Container>;
 };
