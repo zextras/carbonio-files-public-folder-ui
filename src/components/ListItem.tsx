eb013398e0000f344907cf869498ee5e9f7c0ba4
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-10-31 14:34:44 +0100

    fix: remove extra chars

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-import {Avatar, Padding, Text, useTheme} from "@zextras/carbonio-design-system";
-import {NodeType} from "../types/graphql/types.ts";
+import {
+  Avatar,
+  Padding,
+  Text,
+  useTheme,
+} from "@zextras/carbonio-design-system";
+import styled from "styled-components";
+
+import { NodeType } from "../types/graphql/types.ts";
 import {
   ICON_BY_NODE_TYPE,
   ICON_COLOR_BY_NODE_TYPE,
 } from "../utils/constants.ts";
 import { humanFileSize } from "../utils/utils.ts";
-
-import styled from "styled-components";
 
 interface ListItemProps {
   name: string;
@@ -40,14 +45,13 @@ export const ListItem: React.FC<ListItemProps> = ({
   return (
 	<>
 	<Padding left={'1.5rem'}>
-	<CustomAvatar label={''} icon={ICON_BY_NODE_TYPE[type](mimeType)} color={ICON_COLOR_BY_NODE_TYPE[type](theme, mimeType)} background={'gray3'} shape={'square'} size={'large'}/>
-        />
+			<CustomAvatar label={''} icon={ICON_BY_NODE_TYPE[type](mimeType)} color={ICON_COLOR_BY_NODE_TYPE[type](theme, mimeType)} background={'gray3'} shape={'square'} size={'large'}/>
       </Padding>
       <Text>{name}</Text>
 	<Text>{Intl.DateTimeFormat(undefined, {day: "2-digit", minute: "2-digit", hour: "2-digit", month: "2-digit", year: "numeric"}).format(new Date(lastModified))}</Text>
       <Text>{extension}</Text>
 	<Padding right={'1.5rem'}>
-	<Text>{size !== undefined ? humanFileSize(size) : '-'}</Text>
+			<Text>{size !== undefined ? humanFileSize(size) : '-'}</Text>
       </Padding>
     </>
   );
