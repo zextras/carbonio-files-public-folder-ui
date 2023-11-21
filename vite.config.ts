9e5c6ed979fdce8e86f701d220cbd67edefea3d4
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-11-21 09:16:25 +0100

    style: lint config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
 	plugins: [react(), splitVendorChunkPlugin()],
 	base: '/files/public/link/access',
 	test: {
@@ -22,4 +22,4 @@ export default defineConfig(({ command }) => ({
 			}
 		}
 	}
-}));
+});
