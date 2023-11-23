cc9172eaba033a6f5c94e5153b1a3bcecf85fcae
Cataldo Mazzilli <cm@zextras.com>
2023-11-23 11:01:57 +0100

    refactor: move requests to client

--- src/hooks/useFindNodes.ts
+++ src/hooks/useFindNodes.ts
@@ -5,15 +5,9 @@
  */
 import { useCallback, useEffect, useState } from 'react';
 
-import { ExecutionResult } from 'graphql/execution';
-import { print } from 'graphql/language';
-
 import { nodesMap, tokenMap } from './NodesCache';
-import { NodeOfFindNodes } from '../components/types';
-import { FindNodesDocument, GQLFindNodesQuery, GQLFindNodesQueryVariables } from '../graphql/types';
-import { Body } from '../mocks/handlers/handlers.learning.test';
-import { convertGQLToNode, Node } from '../model/Node';
-import { API_ENDPOINT, FIND_NODES_LIMITS } from '../utils/constants';
+import { Node } from '../model/Node';
+import { client } from '../network/client';
 
 type UseFindNodesReturnType = {
 	nodes: Array<Node> | null;
@@ -23,80 +17,42 @@ type UseFindNodesReturnType = {
 
 export const useFindNodes = (folderId: string): UseFindNodesReturnType => {
 	const [nodes, setNodes] = useState<Array<Node> | null>(nodesMap[folderId] ?? null);
-	const [hasMore, setHasMore] = useState(false);
 	const [token, setToken] = useState<string | undefined | null>();
 
 	useEffect(() => {
 		setToken(undefined);
-		setHasMore(false);
 	}, [folderId]);
 
 	useEffect(() => {
 		if (nodesMap[folderId] && tokenMap[folderId] !== undefined) {
 			setNodes(nodesMap[folderId]);
 			setToken(tokenMap[folderId]);
-			setHasMore(tokenMap[folderId] !== null);
 			return;
 		}
 
-		const body: Body<GQLFindNodesQueryVariables> = {
-			variables: { folder_id: folderId },
-			query: print(FindNodesDocument)
-		};
-
-		fetch(new URL(API_ENDPOINT, window.location.origin), {
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify(body),
-			method: 'POST'
-		})
-			.then((response): Promise<ExecutionResult<GQLFindNodesQuery>> => response.json())
-			.then((result) => {
-				const newRows = result.data?.findNodes?.nodes
-					.filter((value): value is NodeOfFindNodes => value !== null)
-					.map((node) => convertGQLToNode(node));
-				setNodes(newRows ?? []);
-				nodesMap[folderId] = newRows ?? [];
-				setToken(result.data?.findNodes?.page_token);
-				tokenMap[folderId] = result.data?.findNodes?.page_token ?? null;
-				if (result.data?.findNodes?.page_token) {
-					setHasMore(true);
-				}
-			});
+		client.findNodesQuery(folderId).then(({ newNodes, newToken }) => {
+			setNodes(newNodes);
+			nodesMap[folderId] = newNodes;
+			setToken(newToken);
+			tokenMap[folderId] = newToken;
+		});
 	}, [folderId]);
 
 	const findMore = useCallback(() => {
-		if (!hasMore) {
+		if (token === null) {
 			throw new Error('No more nodes available');
 		}
 		if (folderId === undefined) {
 			throw new Error('Cannot findMore when folderId is not defined');
 		}
-		const body: Body<GQLFindNodesQueryVariables> = {
-			variables: { folder_id: folderId, page_token: token, limit: FIND_NODES_LIMITS },
-			query: print(FindNodesDocument)
-		};
 
-		fetch(new URL(API_ENDPOINT, window.location.origin), {
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify(body),
-			method: 'POST'
-		})
-			.then((response): Promise<ExecutionResult<GQLFindNodesQuery>> => response.json())
-			.then((result) => {
-				const newRows = result.data?.findNodes?.nodes
-					.filter((value): value is NodeOfFindNodes => value !== null)
-					.map((node) => convertGQLToNode(node));
-				setNodes((oldNodes) => [...(oldNodes ?? []), ...(newRows ?? [])]);
-				setToken(result.data?.findNodes?.page_token);
-				setHasMore(!!result.data?.findNodes?.page_token);
-				nodesMap[folderId] = [...(nodesMap[folderId] ?? []), ...(newRows ?? [])];
-				tokenMap[folderId] = result.data?.findNodes?.page_token ?? null;
-			});
-	}, [folderId, hasMore, token]);
+		client.findNodesQuery(folderId, token).then(({ newNodes, newToken }) => {
+			setNodes((oldNodes) => [...(oldNodes ?? []), ...(newNodes ?? [])]);
+			setToken(newToken);
+			nodesMap[folderId] = [...(nodesMap[folderId] ?? []), ...(newNodes ?? [])];
+			tokenMap[folderId] = newToken;
+		});
+	}, [folderId, token]);
 
-	return { nodes, hasMore, findMore };
+	return { nodes, hasMore: token != null, findMore };
 };
---
+++ src/network/client.ts
@@ -0,0 +1,86 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { GraphQLError } from 'graphql/error';
+import { ExecutionResult } from 'graphql/execution';
+import { print } from 'graphql/language';
+
+import { NodeOfFindNodes } from '../components/types';
+import {
+	FindNodesDocument,
+	GetPublicNodeDocument,
+	GQLFindNodesQuery,
+	GQLFindNodesQueryVariables,
+	GQLGetPublicNodeQuery,
+	GQLGetPublicNodeQueryVariables
+} from '../graphql/types';
+import { convertGQLToNode, Node } from '../model/Node';
+import { API_ENDPOINT, FIND_NODES_LIMITS } from '../utils/constants';
+
+export type Body<TVariables extends Record<string, unknown>> = {
+	variables: TVariables;
+	query: string;
+};
+export const client = {
+	findNodesQuery: (
+		folderId: string,
+		token?: string
+	): Promise<{ newNodes: Array<Node>; newToken: string | null }> => {
+		const body: Body<GQLFindNodesQueryVariables> = {
+			variables: { folder_id: folderId, page_token: token, limit: FIND_NODES_LIMITS },
+			query: print(FindNodesDocument)
+		};
+
+		return fetch(new URL(API_ENDPOINT, window.location.origin), {
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify(body),
+			method: 'POST'
+		})
+			.then((response): Promise<ExecutionResult<GQLFindNodesQuery>> => response.json())
+			.then((result) => {
+				const newNodes = result.data?.findNodes?.nodes
+					.filter((value): value is NodeOfFindNodes => value !== null)
+					.map((node) => convertGQLToNode(node));
+				return { newNodes: newNodes ?? [], newToken: result.data?.findNodes?.page_token ?? null };
+			});
+	},
+	getPublicNodeQuery: (
+		nodeLinkId: string
+	): Promise<{
+		publicNode: { id: string; name: string } | undefined;
+		errors: readonly GraphQLError[] | undefined;
+	}> => {
+		const body: Body<GQLGetPublicNodeQueryVariables> = {
+			variables: { node_link_id: nodeLinkId },
+			query: print(GetPublicNodeDocument)
+		};
+
+		return fetch(new URL(API_ENDPOINT, window.location.origin), {
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify(body),
+			method: 'POST'
+		})
+			.then((response): Promise<ExecutionResult<GQLGetPublicNodeQuery>> => response.json())
+			.then((result) => {
+				if (result.data?.getPublicNode?.id && result.data.getPublicNode.name) {
+					return {
+						publicNode: {
+							id: result.data.getPublicNode.id,
+							name: result.data.getPublicNode.name
+						},
+						errors: undefined
+					};
+				}
+				if (result.errors && result.errors.length > 0) {
+					return { publicNode: undefined, errors: result.errors };
+				}
+				throw new Error('Missing data and errors');
+			});
+	}
+};
