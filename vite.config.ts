f6b8c8baf84c97e2fc4bf02173cf8ddfafb0ba55
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-11-23 18:44:39 +0100

    ci: try to add subfolder package on folder package

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,23 @@ export default defineConfig(({ mode }) => {
 			viteStaticCopy({
 				targets: [
 					{
+						src: 'package/PKGBUILD.template',
+						dest: 'package/package',
+						transform(content): string {
+							return content
+								.replace('{{name}}', pkg.name)
+								.replace('{{version}}', pkg.version)
+								.replace('{{rel}}', `${pkgRel}`)
+								.replace('{{description}}', pkg.description);
+						},
+						rename(filename, fileExt): string {
+							if (fileExt === 'template') {
+								return filename;
+							}
+							return `${filename}.${fileExt}`;
+						}
+					},
+					{
 						src: 'package/*',
 						dest: 'package',
 						transform(content): string {
