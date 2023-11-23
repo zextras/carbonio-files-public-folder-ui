05af67514501bf6d53bd39ab9a8d4d66a0286cd6
Cataldo <cm@zextras.com>
2023-11-23 17:50:59 +0100

    feat: implement caching

--- src/hooks/useFindNodes.ts
+++ src/hooks/useFindNodes.ts
@@ -5,14 +5,9 @@
  */
 import { useCallback, useEffect, useState } from 'react';
 
-import { ExecutionResult } from 'graphql/execution';
-import { print } from 'graphql/language';
-
-import { NodeOfFindNodes } from '../components/types';
-import { FindNodesDocument, GQLFindNodesQuery, GQLFindNodesQueryVariables } from '../graphql/types';
-import { Body } from '../mocks/handlers/handlers.learning.test';
-import { convertGQLToNode, Node } from '../model/Node';
-import { API_ENDPOINT, FIND_NODES_LIMITS } from '../utils/constants';
+import { nodesMap, tokenMap } from './NodesCache';
+import { Node } from '../model/Node';
+import { client } from '../network/client';
 
 type UseFindNodesReturnType = {
 	nodes: Array<Node> | null;
@@ -20,74 +15,46 @@ type UseFindNodesReturnType = {
 	findMore: () => void;
 };
 
-export const useFindNodes = (folderId: string | undefined): UseFindNodesReturnType => {
-	const [nodes, setNodes] = useState<Array<Node> | null>(null);
-	const [hasMore, setHasMore] = useState(false);
+export const useFindNodes = (folderId: string): UseFindNodesReturnType => {
+	const [nodes, setNodes] = useState<Array<Node> | null>(nodesMap.get(folderId) ?? null);
 	const [token, setToken] = useState<string | undefined | null>();
 
 	useEffect(() => {
 		setToken(undefined);
-		setHasMore(false);
 	}, [folderId]);
 
 	useEffect(() => {
-		if (folderId === undefined) {
+		const cachedNodes = nodesMap.get(folderId);
+		const cachedToken = tokenMap.get(folderId);
+		if (cachedNodes && cachedToken !== undefined) {
+			setNodes(cachedNodes);
+			setToken(cachedToken);
 			return;
 		}
-		const body: Body<GQLFindNodesQueryVariables> = {
-			variables: { folder_id: folderId },
-			query: print(FindNodesDocument)
-		};
 
-		fetch(new URL(API_ENDPOINT, window.location.origin), {
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify(body),
-			method: 'POST'
-		})
-			.then((response): Promise<ExecutionResult<GQLFindNodesQuery>> => response.json())
-			.then((result) => {
-				const newRows = result.data?.findNodes?.nodes
-					.filter((value): value is NodeOfFindNodes => value !== null)
-					.map((node) => convertGQLToNode(node));
-				setNodes(newRows ?? []);
-				setToken(result.data?.findNodes?.page_token);
-				if (result.data?.findNodes?.page_token) {
-					setHasMore(true);
-				}
-			});
+		client.findNodesQuery(folderId).then(({ newNodes, newToken }) => {
+			setNodes(newNodes);
+			nodesMap.set(folderId, newNodes);
+			setToken(newToken);
+			tokenMap.set(folderId, newToken);
+		});
 	}, [folderId]);
 
 	const findMore = useCallback(() => {
-		if (!hasMore) {
+		if (token === null) {
 			throw new Error('No more nodes available');
 		}
 		if (folderId === undefined) {
 			throw new Error('Cannot findMore when folderId is not defined');
 		}
-		const body: Body<GQLFindNodesQueryVariables> = {
-			variables: { folder_id: folderId, page_token: token, limit: FIND_NODES_LIMITS },
-			query: print(FindNodesDocument)
-		};
 
-		fetch(new URL(API_ENDPOINT, window.location.origin), {
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify(body),
-			method: 'POST'
-		})
-			.then((response): Promise<ExecutionResult<GQLFindNodesQuery>> => response.json())
-			.then((result) => {
-				const newRows = result.data?.findNodes?.nodes
-					.filter((value): value is NodeOfFindNodes => value !== null)
-					.map((node) => convertGQLToNode(node));
-				setNodes((oldNodes) => [...(oldNodes ?? []), ...(newRows ?? [])]);
-				setToken(result.data?.findNodes?.page_token);
-				setHasMore(!!result.data?.findNodes?.page_token);
-			});
-	}, [folderId, hasMore, token]);
+		client.findNodesQuery(folderId, token).then(({ newNodes, newToken }) => {
+			setNodes((oldNodes) => [...(oldNodes ?? []), ...(newNodes ?? [])]);
+			setToken(newToken);
+			nodesMap.set(folderId, [...(nodesMap.get(folderId) ?? []), ...(newNodes ?? [])]);
+			tokenMap.set(folderId, newToken);
+		});
+	}, [folderId, token]);
 
-	return { nodes, hasMore, findMore };
+	return { nodes, hasMore: token != null, findMore };
 };
